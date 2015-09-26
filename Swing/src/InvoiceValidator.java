f781af86b9e80226eabf425f01509de0e59f0852
ParthibanRajalingam <parthiban.rajalingam@gmail.com>
2015-09-26 23:30:49 +0530

    Hello
    Im batman

---
+++ Swing/src/InvoiceValidator.java
@@ -0,0 +1,134 @@
+import javax.swing.*;
+import controller.*;
+import javax.swing.filechooser.*;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.xml.sax.SAXException;
+
+import java.awt.*;
+import java.awt.event.*;
+import java.io.File;
+import java.io.IOException;
+
+public class InvoiceValidator
+{
+
+/*	public String Fetcher(){
+		return path+phoneNumber;
+	}*/
+	public static void main(String a[]) throws ParserConfigurationException, SAXException, IOException
+	{
+		JFrame frame = new JFrame();
+		
+		JLabel xmlopen, msisdn;
+
+		final JTextField xmlfilename;
+
+		final JTextField phone;
+		String path="";
+		String phoneNumber="";
+
+		JButton browse, submit, reset;
+
+			Container contentPane = frame.getContentPane();
+	        	SpringLayout layout = new SpringLayout();
+			contentPane.setLayout(layout);
+			
+			xmlopen = new JLabel("XML File");
+			contentPane.add(xmlopen);
+
+			xmlfilename = new JTextField(20);
+			contentPane.add(xmlfilename);			
+
+			browse = new JButton("Browse");
+			contentPane.add(browse);
+
+			browse.addActionListener(new ActionListener()
+			{
+				public void actionPerformed(ActionEvent ae)
+				{
+					JFileChooser fc = new JFileChooser();
+					fc.setFileSelectionMode(fc.FILES_AND_DIRECTORIES);
+					FileNameExtensionFilter xmlfilter = new FileNameExtensionFilter("xml files", "xml");
+					fc.setFileFilter(xmlfilter);
+					int returnVal = fc.showOpenDialog(null);
+					if(returnVal == JFileChooser.APPROVE_OPTION)
+					{
+						fc.setMultiSelectionEnabled(false);
+						xmlfilename.setText(fc.getSelectedFile().getAbsolutePath());	
+					}	
+				}
+			});
+			
+			msisdn = new JLabel("MSISDN");
+			contentPane.add(msisdn);
+
+			phone = new JTextField(20);
+			contentPane.add(phone);
+
+			submit = new JButton("Submit");
+			contentPane.add(submit);
+
+			reset = new JButton("Reset");
+			contentPane.add(reset);
+			reset.addActionListener(new ActionListener()
+			{
+				public void actionPerformed(ActionEvent ae)
+				{
+					xmlfilename.setText("");
+					phone.setText("");
+				}
+			});
+			
+			submit.addActionListener(new ActionListener()
+			{
+				public void actionPerformed(ActionEvent ae)
+				{
+				String path=xmlfilename.getText();
+				String phoneNumber=phone.getText();
+				System.out.println(path+phoneNumber);
+				try {
+					controller.summary.summary(path, phoneNumber);
+				} catch (ParserConfigurationException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (SAXException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				}
+				
+			});
+
+			frame.setSize(400,130);
+
+			layout.putConstraint(SpringLayout.WEST, xmlopen, 5, SpringLayout.WEST, contentPane);
+			layout.putConstraint(SpringLayout.NORTH, xmlopen, 7, SpringLayout.NORTH, contentPane);
+
+			layout.putConstraint(SpringLayout.WEST, xmlfilename, 10, SpringLayout.EAST, xmlopen);
+			layout.putConstraint(SpringLayout.NORTH, xmlfilename, 7, SpringLayout.NORTH, contentPane);
+
+			layout.putConstraint(SpringLayout.WEST, browse, 10, SpringLayout.EAST, xmlfilename);
+			layout.putConstraint(SpringLayout.NORTH, browse, 5, SpringLayout.NORTH, contentPane);
+
+			layout.putConstraint(SpringLayout.WEST, msisdn, 5, SpringLayout.WEST, contentPane);
+			layout.putConstraint(SpringLayout.NORTH, msisdn, 20, SpringLayout.SOUTH, xmlopen);
+
+			layout.putConstraint(SpringLayout.WEST, phone, 13, SpringLayout.EAST, msisdn);
+			layout.putConstraint(SpringLayout.NORTH, phone, 15, SpringLayout.SOUTH, xmlfilename);
+			
+			layout.putConstraint(SpringLayout.WEST, submit, 100, SpringLayout.WEST, contentPane);
+			layout.putConstraint(SpringLayout.NORTH, submit, 15, SpringLayout.SOUTH, phone);
+
+			layout.putConstraint(SpringLayout.WEST, reset, 13, SpringLayout.EAST, submit);
+			layout.putConstraint(SpringLayout.NORTH, reset, 15, SpringLayout.SOUTH, phone);
+
+			frame.setVisible(true);
+			frame.setTitle("Invoice Validator");
+			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			//System.out.println(path+phoneNumber);
+	}
+}
---
+++ Swing/src/controller/summary.java
@@ -0,0 +1,76 @@
+package controller;
+
+import java.awt.List;
+import java.io.File;
+import java.io.IOException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+public class summary {
+	public static void summary(String path, String msisdn)
+			throws ParserConfigurationException, SAXException, IOException {
+		File xmlFile = new File(path); // getting the xml file
+		DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+		Document doc = dBuilder.parse(xmlFile); // parsinfg xml file
+		int key=0;// value to store when at which iteration the msisdn has been
+		String find="notFound";		// found
+
+	NodeList nList = doc.getElementsByTagName("INVOICE_SPECIFICATION"); // creating
+																			// a
+																			// list
+																			// with
+																			// the
+																			// values
+																			// under
+																			// invoice_spec
+																			// tags
+ NodeList nBigList= doc.getElementsByTagName("INVOICE");//Parsing all tags 
+		for (int temp = 0; temp < nList.getLength(); temp++) { // TO check all d
+																// occurrence of
+																// inv_spec tag
+
+			Node nNode = nList.item(temp);// accessing the first(temp's) node
+											// branch
+
+			if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+
+				Element eElement = (Element) nNode;
+				if ((msisdn).equals(eElement.getElementsByTagName("MSISDN")
+						.item(0).getTextContent())) {
+					key = temp;
+					find="found";
+				}
+			}
+		}
+		if(find.equals("found")){
+		Node nBilledPeriod= nBigList.item(0); //Fetch bill period from the big list.
+		if(nBilledPeriod.getNodeType()==Node.ELEMENT_NODE){
+			Element eElement = (Element) nBilledPeriod;
+			System.out.println("Billing Period Of the selected MSISDN: "
+					+ eElement.getElementsByTagName("BILL_CYCLE").item(0)
+							.getTextContent());
+		}
+		Node nPlan = nList.item(key);//fetching plan name from small list
+		if (nPlan.getNodeType() == Node.ELEMENT_NODE) {
+
+			Element eElement = (Element) nPlan;
+			System.out.println("PLAN Name Of the selected MSISDN: "
+					+ eElement.getElementsByTagName("PLAN_NAME").item(0)
+							.getTextContent());
+
+		}
+		}
+		if(find.equals("notFound")){
+			System.out.println("Msisdn is not present in this invoice. Please check the msisdn.");
+		}
+	}
+}
